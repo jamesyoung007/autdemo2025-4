2cd4875259bf28c1fc0e295e67741da80ad744b8
shaoniaogong <shaoniaogong@outlook.com>
2025-05-18 21:05:20 +1200

    updates

--- main.tf
+++ main.tf
@@ -10,7 +10,7 @@ resource "azurerm_resource_group" "main" {
 
 # App Service Plan using CAF module
 module "app_service_plan" {
-  source              = "github.com/aztfmod/terraform-azurerm-caf//modules/app_service_plan?ref=5.7.14"
+  source              = "github.com/aztfmod/terraform-azurerm-caf//modules/compute/app_service_plan?ref=5.7.14"
   resource_group_name = azurerm_resource_group.main.name
   location            = azurerm_resource_group.main.location
   name                = var.app_service_plan_name
@@ -18,9 +18,8 @@ module "app_service_plan" {
 }
 
 # Storage Account using CAF module
-<<<<<<< HEAD
 module "storage_account" {
-  source                    = "github.com/aztfmod/terraform-azurerm-caf//modules/storage_account?ref=5.7.14"
+  source                    = "github.com/aztfmod/terraform-azurerm-caf//modules/databases/storage_account?ref=5.7.14"
   resource_group_name       = azurerm_resource_group.main.name
   location                  = azurerm_resource_group.main.location
   name                      = var.storage_account_name
@@ -30,7 +29,7 @@ module "storage_account" {
 
 # Log Analytics Workspace using CAF module
 module "log_analytics" {
-  source              = "github.com/aztfmod/terraform-azurerm-caf//modules/log_analytics_workspace?ref=5.7.14"
+  source              = "github.com/aztfmod/terraform-azurerm-caf//modules/operations/log_analytics_workspace?ref=5.7.14"
   resource_group_name = azurerm_resource_group.main.name
   location            = azurerm_resource_group.main.location
   name                = var.log_analytics_workspace_name
@@ -39,7 +38,7 @@ module "log_analytics" {
 
 # Function App using CAF module
 module "function_app" {
-  source                      = "github.com/aztfmod/terraform-azurerm-caf//modules/function_app?ref=4.22.0"
+  source                      = "github.com/aztfmod/terraform-azurerm-caf//modules/compute/function_app?ref=4.22.0"
   resource_group_name         = azurerm_resource_group.main.name
   location                    = azurerm_resource_group.main.location
   name                        = var.function_app_name
@@ -50,36 +49,3 @@ module "function_app" {
   os_type                     = "linux"
   version                     = "~> 4.0"
 }
-=======
-#module "storage_account" {
-#  source                    = "aztfmod/caf/azurerm//modules/storage_account"
-#  resource_group_name       = azurerm_resource_group.main.name
-#  location                  = azurerm_resource_group.main.location
-#  name                      = var.storage_account_name
-#  account_tier              = var.storage_account_tier
-#  account_replication_type  = var.storage_account_replication
-#}
-
-# Log Analytics Workspace using CAF module
-#module "log_analytics" {
-  #source              = "aztfmod/caf/azurerm//modules/log_analytics_workspace"
-  #resource_group_name = azurerm_resource_group.main.name
- # location            = azurerm_resource_group.main.location
- # name                = var.log_analytics_workspace_name
- # sku                 = var.log_analytics_workspace_sku
-#}
-
-# Function App using CAF module
-#module "function_app" {
- # source                      = "aztfmod/caf/azurerm//modules/function_app"
- # resource_group_name         = azurerm_resource_group.main.name
- # location                    = azurerm_resource_group.main.location
-  #name                        = var.function_app_name
- # app_service_plan_id         = module.app_service_plan.id
- # storage_account_name        = module.storage_account.name
- # storage_account_access_key  = module.storage_account.primary_access_key
-#  application_insights_key    = module.log_analytics.instrumentation_key
-#  os_type                     = "linux"
-#  version                     = "~> 4.0"
-#}
->>>>>>> 5c45a5430e83de6c98e906fa0d6bef06178dfc0e
