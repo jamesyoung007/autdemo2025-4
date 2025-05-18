a16b6aaf396066002074d6912c7c3491f64d8d42
shaoniaogong <shaoniaogong@outlook.com>
2025-05-18 21:09:53 +1200

    updates

--- main.tf
+++ main.tf
@@ -2,50 +2,65 @@ provider "azurerm" {
   features {}
 }
 
-# Resource Group
 resource "azurerm_resource_group" "main" {
   name     = var.resource_group_name
   location = var.location
 }
 
-# App Service Plan using CAF module
-module "app_service_plan" {
-  source              = "github.com/aztfmod/terraform-azurerm-caf//modules/compute/app_service_plan?ref=5.7.14"
-  resource_group_name = azurerm_resource_group.main.name
-  location            = azurerm_resource_group.main.location
-  name                = var.app_service_plan_name
-  sku_name            = var.app_service_plan_sku
-}
+module "caf" {
+  source  = "aztfmod/caf/azurerm"
+  version = "5.7.14"
 
-# Storage Account using CAF module
-module "storage_account" {
-  source                    = "github.com/aztfmod/terraform-azurerm-caf//modules/databases/storage_account?ref=5.7.14"
-  resource_group_name       = azurerm_resource_group.main.name
-  location                  = azurerm_resource_group.main.location
-  name                      = var.storage_account_name
-  account_tier              = var.storage_account_tier
-  account_replication_type  = var.storage_account_replication
-}
+  # Resource group context
+  resource_groups = {
+    demo = {
+      name     = var.resource_group_name
+      location = var.location
+    }
+  }
 
-# Log Analytics Workspace using CAF module
-module "log_analytics" {
-  source              = "github.com/aztfmod/terraform-azurerm-caf//modules/operations/log_analytics_workspace?ref=5.7.14"
-  resource_group_name = azurerm_resource_group.main.name
-  location            = azurerm_resource_group.main.location
-  name                = var.log_analytics_workspace_name
-  sku                 = var.log_analytics_workspace_sku
-}
+  # App Service Plan
+  app_service_plans = {
+    demo_plan = {
+      name                = var.app_service_plan_name
+      resource_group_key  = "demo"
+      location            = var.location
+      sku_name            = var.app_service_plan_sku
+    }
+  }
 
-# Function App using CAF module
-module "function_app" {
-  source                      = "github.com/aztfmod/terraform-azurerm-caf//modules/compute/function_app?ref=4.22.0"
-  resource_group_name         = azurerm_resource_group.main.name
-  location                    = azurerm_resource_group.main.location
-  name                        = var.function_app_name
-  app_service_plan_id         = module.app_service_plan.id
-  storage_account_name        = module.storage_account.name
-  storage_account_access_key  = module.storage_account.primary_access_key
-  application_insights_key    = module.log_analytics.instrumentation_key
-  os_type                     = "linux"
-  version                     = "~> 4.0"
+  # Storage Account
+  storage_accounts = {
+    demo_storage = {
+      name                     = var.storage_account_name
+      resource_group_key       = "demo"
+      location                 = var.location
+      account_tier             = var.storage_account_tier
+      account_replication_type = var.storage_account_replication
+    }
+  }
+
+  # Log Analytics Workspace
+  log_analytics_workspaces = {
+    demo_law = {
+      name               = var.log_analytics_workspace_name
+      resource_group_key = "demo"
+      location           = var.location
+      sku                = var.log_analytics_workspace_sku
+    }
+  }
+
+  # Function App
+  function_apps = {
+    demo_func = {
+      name                       = var.function_app_name
+      resource_group_key         = "demo"
+      location                   = var.location
+      app_service_plan_key       = "demo_plan"
+      storage_account_key        = "demo_storage"
+      log_analytics_workspace_key = "demo_law"
+      os_type                    = "linux"
+      version                    = "~4"
+    }
+  }
 }
