f67f473c3fbaf7c09d01fd4cb5de40ee0c264bf9
shaoniaogong <shaoniaogong@outlook.com>
2025-05-18 21:21:34 +1200

    3

--- main.tf
+++ main.tf
@@ -7,44 +7,41 @@ resource "azurerm_resource_group" "main" {
   location = var.location
 }
 
-module "app_service_plan" {
-  source              = "aztfmod/caf/azurerm//modules/app_service_plan"
-  version             = "5.7.14"
-  resource_group_name = azurerm_resource_group.main.name
-  location            = azurerm_resource_group.main.location
+resource "azurerm_service_plan" "plan" {
   name                = var.app_service_plan_name
+  location            = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
+  os_type             = "Linux"
   sku_name            = var.app_service_plan_sku
 }
 
-module "storage_account" {
-  source                    = "aztfmod/caf/azurerm//modules/storage_account"
-  version                   = "5.7.14"
-  resource_group_name       = azurerm_resource_group.main.name
-  location                  = azurerm_resource_group.main.location
-  name                      = var.storage_account_name
-  account_tier              = var.storage_account_tier
-  account_replication_type  = var.storage_account_replication
+resource "azurerm_storage_account" "storage" {
+  name                     = var.storage_account_name
+  resource_group_name      = azurerm_resource_group.main.name
+  location                 = azurerm_resource_group.main.location
+  account_tier             = var.storage_account_tier
+  account_replication_type = var.storage_account_replication
 }
 
-module "log_analytics" {
-  source              = "aztfmod/caf/azurerm//modules/log_analytics_workspace"
-  version             = "5.7.14"
-  resource_group_name = azurerm_resource_group.main.name
-  location            = azurerm_resource_group.main.location
+resource "azurerm_log_analytics_workspace" "law" {
   name                = var.log_analytics_workspace_name
+  location            = var.location
+  resource_group_name = var.resource_group_name
   sku                 = var.log_analytics_workspace_sku
+  retention_in_days   = 30
 }
 
-module "function_app" {
-  source                      = "aztfmod/caf/azurerm//modules/function_app"
-  version                     = "5.7.14"
-  resource_group_name         = azurerm_resource_group.main.name
-  location                    = azurerm_resource_group.main.location
-  name                        = var.function_app_name
-  app_service_plan_id         = module.app_service_plan.id
-  storage_account_name        = module.storage_account.name
-  storage_account_access_key  = module.storage_account.primary_access_key
-  application_insights_key    = module.log_analytics.instrumentation_key
-  os_type                     = "linux"
-  version_function            = "~4"
+resource "azurerm_linux_function_app" "function" {
+  name                       = var.function_app_name
+  location                   = azurerm_resource_group.main.location
+  resource_group_name        = azurerm_resource_group.main.name
+  service_plan_id            = azurerm_service_plan.plan.id
+  storage_account_name       = azurerm_storage_account.storage.name
+  storage_account_access_key = azurerm_storage_account.storage.primary_access_key
+
+  site_config {
+    application_stack {
+      node_version = "18"
+    }
+  }
 }
