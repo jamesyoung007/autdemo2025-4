5967a7a6ad7001b2f52c259fd724cc534789ed10
shaoniaogong <shaoniaogong@outlook.com>
2025-05-19 23:35:27 +1200

    12

--- main.tf
+++ main.tf
@@ -1,4 +1,10 @@
 terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = ">= 3.0.0"
+    }
+  }
   backend "azurerm" {
     resource_group_name   = "tfstaterg"
     storage_account_name  = "autdemotfstate"
